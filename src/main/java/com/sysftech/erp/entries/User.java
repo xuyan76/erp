5951cf02490a71deeb0c35d7e0957721cd8a9d13
xuyan <3426373@qq.com>
2024-06-04 22:15:00 +0800

    init commit

--- src/main/java/com/sysftech/erp/entries/User.java
+++ src/main/java/com/sysftech/erp/entries/User.java
@@ -1,6 +1,8 @@
 package com.sysftech.erp.entries;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -11,6 +13,7 @@ import java.time.LocalDateTime;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private LocalDateTime createTime;
