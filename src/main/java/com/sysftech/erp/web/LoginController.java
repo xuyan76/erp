493c26e4068261afe8713e38fd63efdb8689600b
xuyan <3426373@qq.com>
2024-06-05 12:49:12 +0800

    init commit

---
+++ src/main/java/com/sysftech/erp/web/LoginController.java
@@ -0,0 +1,16 @@
+package com.sysftech.erp.web;
+
+import com.sysftech.erp.service.SystemService;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/system")
+public class LoginController {
+
+    @Resource
+    private SystemService systemService;
+}
