1bfaccd960eeb715590d05a04f098d51802dbe67
xuyan <3426373@qq.com>
2024-06-15 10:14:38 +0800

    今天开始得写注释了

---
+++ src/main/java/com/sysftech/erp/member/entity/RealName.java
@@ -0,0 +1,26 @@
+package com.sysftech.erp.member.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * 实名认证信息
+ * @author xuyan
+ * @since 2024-06-15
+ */
+@Entity
+@Table(name = "cms_real_name")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class RealName {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String realName;
+    String idCard;
+    LocalDateTime createTime;
+}
---
+++ src/main/java/com/sysftech/erp/product/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.sysftech.erp.product.service;
+
+import com.sysftech.erp.product.repository.ProductCategoryRepository;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ProductService {
+    @Resource
+    ProductCategoryRepository productCategoryDao;
+}
